9fe73e2e4f6f27b3995d814aac90718392fe927e
bobbyrinaldo29 <bobbyrinaldo@icloud.com>
2021-11-08 12:42:09 +0700

    add favicon and other

--- resources/views/dashboard/layouts/partials/head.blade.php
+++ resources/views/dashboard/layouts/partials/head.blade.php
@@ -1,21 +1,20 @@
 <head>
     <meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="keywords" content="" />
-	<meta name="author" content="" />
+	<meta name="keywords" content="nayagaedu forex broker" />
+	<meta name="author" content="NayagaEdu" />
 	<meta name="robots" content="" />
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta name="description" content="Fillow : Fillow Saas Admin  Bootstrap 5 Template" />
-	<meta property="og:title" content="Fillow : Fillow Saas Admin  Bootstrap 5 Template" />
-	<meta property="og:description" content="Fillow : Fillow Saas Admin  Bootstrap 5 Template" />
-	<meta property="og:image" content="https:/fillow.dexignlab.com/xhtml/social-image.png" />
+	<meta name="description" content="Nayaga Edu : Nayaga education center" />
+	<meta property="og:title" content="Nayaga Edu : Nayaga education center" />
+	<meta property="og:description" content="Nayaga Edu : Nayaga education center" />
 	<meta name="format-detection" content="telephone=no">
 	
 	<!-- PAGE TITLE HERE -->
 	<title>{{ config('app.name') }}</title>
 	
 	<!-- FAVICONS ICON -->
-	<link rel="icon" href="{{ asset('/favicon.ico') }}">
+	<link rel="icon" href="{{ asset('/favicon.png') }}">
 	<link href="{{ asset('vendor/jquery-nice-select/css/nice-select.css') }}" rel="stylesheet" />
 	<link href="{{ asset('vendor/owl.carousel/owl.carousel.css') }}" rel="stylesheet" />
 	<link rel="stylesheet" href="{{ asset('vendor/nouislider/nouislider.min.css') }}" />
